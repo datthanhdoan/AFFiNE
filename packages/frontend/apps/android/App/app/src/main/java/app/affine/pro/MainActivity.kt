752cf34f33db47721d013d2fab868029781b1132
Aki <zeatualchang@gmail.com>
2025-01-17 18:29:39 +0800

    fix(android): fix android email sign-in

--- packages/frontend/apps/android/App/app/src/main/java/app/affine/pro/MainActivity.kt
+++ packages/frontend/apps/android/App/app/src/main/java/app/affine/pro/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import com.getcapacitor.BridgeActivity
+import com.getcapacitor.plugin.CapacitorCookies
+import com.getcapacitor.plugin.CapacitorHttp
 
 class MainActivity : BridgeActivity() {
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        registerPlugin(CapacitorHttp::class.java)
+        registerPlugin(CapacitorCookies::class.java)
     }
 }
