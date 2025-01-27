0d7d8aeaab362a27b9ffc23ebe728a1508f57833
xiaoxin <hongtao.lye@toeverything.info>
2025-01-27 17:38:21 +0800

    fix: drag block issue

--- blocksuite/affine/widget-drag-handle/src/watchers/drag-event-watcher.ts
+++ blocksuite/affine/widget-drag-handle/src/watchers/drag-event-watcher.ts
@@ -665,7 +665,7 @@ export class DragEventWatcher {
         onDrop: () => {
           this._cleanup();
         },
-        setDragPreview: ({ source, container }) => {
+        setDragPreview: ({ source, container, setOffset }) => {
           if (!source.data?.bsEntity?.modelIds.length) {
             return;
           }
@@ -674,6 +674,9 @@ export class DragEventWatcher {
             source.data?.bsEntity?.modelIds,
             container
           );
+
+          const rect = container.getBoundingClientRect();
+          setOffset({ x: rect.width / 2, y: rect.height / 2 });
         },
         setDragData: () => {
           const { snapshot } = this._getSnapshotFromHoveredBlocks();
